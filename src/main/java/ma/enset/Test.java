db21b4d01d4a8a68e7252ac4eb268410e1528a68
abc <abc@example.com>
2023-10-22 23:58:50 +0100

    comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,6 +1,10 @@
+/**
+ * commentaires
+ */
 package ma.enset;
 public class Test{
     public static void main (String[] args){
+        //--------------------COMMENTS------------------------
         System.out.println("Hello!!");
     }
 }
